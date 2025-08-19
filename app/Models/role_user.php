e98b7bed71d52dfe8f7ff0d52a8da35dd97c6f2c
Estefany-Viveros <estefany17viveros@gmail.com>
2025-08-19 12:25:25 -0500

    ceraciones de las migraciones y los modelos

--- app/Models/role_user.php
+++ app/Models/role_user.php
@@ -9,4 +9,18 @@ class role_user extends Model
 {
     /** @use HasFactory<\Database\Factories\RoleUserFactory> */
     use HasFactory;
+      protected $fillable = [
+        'user_id',
+        'role_id',
+    ];
+public function user()
+{
+    return $this->belongsTo(User::class);
+}
+
+public function role()
+{
+    return $this->belongsTo(Role::class);
+}
+
 }
